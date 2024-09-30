2e3ab11a104109ca41e415350fd9c7f66a6b4bae
Jake Enea <jakemenea@gmail.com>
2024-09-30 12:14:25 -0400

    refactoring code to work with consumption model

--- function_app.py
+++ function_app.py
@@ -1,27 +1,24 @@
 import logging
 import azure.functions as func
-import feedparser
-import pandas
-from datetime import datetime
-import time
+from datetime import datetime, timezone
 import os
-from azure.storage.blob import BlobServiceClient
 from io import StringIO
 import smtplib
 from email.mime.text import MIMEText
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
+import urllib.request
+import xml.etree.ElementTree as ET
+import csv
 
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 0 13 * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False) 
+@app.schedule(schedule="0 0 12 * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
-
-    connection_string = os.getenv("STORAGEACCOUNT_CONNECTIONSTRING")
 
     urls = []
     with open("feeds.txt", "r") as f:
@@ -29,40 +26,30 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
             urls.append(line)
 
 
-    feeds = {"Source": [], "Title": [], "Description": [], "Link": []}
+    feeds = {"Source": [], "Date": [], "Title": [], "Description": [], "Link": []}
     for url in urls:
-        feed = feedparser.parse(url)
-        feedTitle = feed.feed.title
+        response = urllib.request.urlopen(url)
+        xml_data = response.read()
+        feed = ET.fromstring(xml_data).find('channel')
+        feedTitle = feed.find('title').text
         i = 0
-        while i < len(feed.entries):
-            t1 = datetime.now()
-            t2 = datetime.fromtimestamp(time.mktime(feed.entries[i].published_parsed))
-            if (t1 - t2).days < 1:
+        for item in feed.findall('item'):
+            t1 = datetime.now().astimezone(timezone.utc)
+            t2 = datetime.strptime(item.find('pubDate').text, "%a, %d %b %Y %H:%M:%S %z").astimezone(timezone.utc)
+            if ((t1 - t2).days) < 1:
+                print((str(item.find('description').text).encode("utf-8"))[:150])
                 feeds["Source"].append(feedTitle)
-                feeds["Title"].append(feed.entries[i].title)
-                feeds["Description"].append((str(feed.entries[i].description).encode("utf-8"))[:150])
-                feeds["Link"].append(feed.entries[i].link)
-            i = i + 1
+                feeds["Date"].append(item.find('pubDate').text)
+                feeds["Title"].append(item.find('title').text)
+                feeds["Description"].append((str(item.find('description').text).encode("utf-8"))[:150])
+                feeds["Link"].append(item.find('link').text)
 
-    df = pandas.DataFrame(data=feeds)
+    # write csv data to buffer
     csv_buffer = StringIO()
-    df.to_csv(csv_buffer, index=False)
-
-    # Set up your connection string, container name, and blob name
-    container_name = "func-output"
-    blob_name = "feed.csv"
-
-    # Initialize the BlobServiceClient
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-
-    # Get the container client
-    container_client = blob_service_client.get_container_client(container_name)
-
-    # Get the BlobClient to upload the file
-    blob_client = container_client.get_blob_client(blob_name)
-
-    # Upload the file
-    blob_client.upload_blob(csv_buffer.getvalue(), overwrite=True)
+    keys = feeds.keys()
+    writer = csv.writer(csv_buffer)
+    writer.writerow(keys)
+    writer.writerows(zip(*feeds.values()))
 
     # send email
     subject = "Daily SOC RSS Feed"
@@ -87,5 +74,8 @@ def timer_trigger(myTimer: func.TimerRequest) -> None:
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
         smtp_server.login(sender_email, sender_password)
         smtp_server.sendmail(sender_email, recipient_email, msg.as_string())
+
+    # close csv buffer
+    csv_buffer.close()
 
     logging.info('Python timer trigger function executed.')
