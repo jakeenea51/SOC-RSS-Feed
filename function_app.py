ca62b4e4f946229a991804023f5c81d56f1b781d
Jake Enea <jakemenea@gmail.com>
2024-10-01 08:16:32 -0400

    setting run on startup to false

--- function_app.py
+++ function_app.py
@@ -21,7 +21,7 @@ timezones = {
     "GMT": "+0000",
     }
 
-@app.schedule(schedule="0 0 12 * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False) 
+@app.schedule(schedule="0 0 12 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
